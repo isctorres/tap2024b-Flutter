bbafcbd99f6de3c276dcd3e961d6b31a0cec1a73
Ruben Torres Frias <isctorres@gmail.com>
2024-11-14 18:40:22 -0600

    consumo de api

--- lib/network/api_popular.dart
+++ lib/network/api_popular.dart
@@ -5,7 +5,7 @@ class ApiPopular {
 
   final dio = Dio();
   Future<List<PopularModel>> getAllPopular() async {
-    const URL = "https://api.themoviedb.org/3/movie/popular?api_key=5019e68de7bc112f4e4337a500b96c56&language=es-MX&page=1";
+    const URL = "https://api.themoviedb.org/3/movie/popular?api_key=&language=es-MX&page=1";
     final response = await dio.get(URL);
     final res = response.data['results'] as List; 
     return res.map((movie) => PopularModel.fromMap(movie)).toList();
